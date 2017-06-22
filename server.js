9d46d916276291ba866035d2f60eb94b88eee076
Sushmita Chaudhari <chaudhari.su@husky.neu.edu>
2017-06-22 18:26:24 -0400

    server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(app.express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
-// require("./assignment/app");
+require("./assignment/app");
 require("./project/app");
 
 var port = process.env.PORT || 3000;
